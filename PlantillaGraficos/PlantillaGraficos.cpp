96f238d9e91d74b04c297e2d4a0f9f45da095c24
Armando Meza Leyva <armezaleyva@gmail.com>
2020-03-06 10:48:48 -0700

    Mapeo de atributos

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -96,6 +96,33 @@ int main() {
 	const char* rutaFragmentShader = "FragmentShader.shader";
 	shader = new Shader(rutaVertexShader, rutaFragmentShader);
 
+	// Mapeo de atributos
+	posicionID = glGetAttribLocation(shader->getID, "posicion");
+	colorID = glGetAttribLocation(shader->getID, "color");
+
+	shader->desenlazar();
+
+	// Crear el vertex array del triángulo
+	glGenVertexArrays(1, &vertexArrayTrianguloID);
+	glBindVertexArray(vertexArrayTrianguloID);
+
+	// Vertex buffer
+	glGenBuffers(1, &bufferTrianguloID);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferTrianguloID);
+
+	// Asociar datos al buffer
+	glBufferData(
+		GL_ARRAY_BUFFER, sizeof(Vertice) * triangulo.size(),
+		triangulo.data(), GL_STATIC_DRAW
+	);
+
+	// Habilitar atributos del shader
+	glEnableVertexAttribArray(posicionID);
+	glEnableVertexAttribArray(colorID);
+
+	// Especificar a OpenGL como comunicarse
+	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+
 	// Ciclo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window)) {
 		// Establecer región de dibujo
