f7d2ea57e0245454c4ac0c9776ee8346ac664a65
Armando Meza Leyva <armezaleyva@gmail.com>
2020-03-10 08:51:35 -0700

    Clase

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -25,6 +25,10 @@ vector<Vertice> triangulo;
 GLuint vertexArrayTrianguloID;
 GLuint bufferTrianguloID;
 
+vector<Vertice> cuadrado;
+GLuint vertexArrayCuadradoID;
+GLuint bufferCuadradoID;
+
 //Instancia del shader
 Shader* shader;
 //Identificadores para mapeo de atributos de entrada
@@ -32,6 +36,32 @@ Shader* shader;
 GLuint posicionID;
 GLuint colorID;
 
+void inicializarCuadrado() {
+	Vertice v1 = {
+		vec3(-0.2f, 0.2f, 0.0f),
+		vec4(0.1f, 0.8f, 0.2f, 1.0f)
+	};
+
+	Vertice v2 = {
+		vec3(0.2f, 0.2f, 0.0f),
+		vec4(0.1f, 0.8f, 0.2f, 1.0f),
+	};
+
+	Vertice v3 = {
+		vec3(0.2f, -0.2f, 0.0f),
+		vec4(0.1f, 0.8f, 0.2f, 1.0f),
+	};
+
+	Vertice v4 = {
+		vec3(-0.2f, -0.2f, 0.0f),
+		vec4(0.1f, 0.8f, 0.2f, 1.0f),
+	};
+
+	cuadrado.push_back(v1);
+	cuadrado.push_back(v2);
+	cuadrado.push_back(v3);
+	cuadrado.push_back(v4);
+}
 
 void inicializarTriangulo() {
 	Vertice v1 = {
@@ -58,6 +88,10 @@ void dibujar() {
 	glBindVertexArray(vertexArrayTrianguloID);
 	//Dibujar
 	glDrawArrays(GL_TRIANGLES, 0, triangulo.size());
+
+	// Proceso dibujo de cuadrado
+	glBindVertexArray(vertexArrayCuadradoID);
+	glDrawArrays(GL_QUADS, 0, cuadrado.size());
 
 	//Soltar vertex array
 	glBindVertexArray(0);
@@ -103,18 +137,15 @@ int main()
 	cout << "Version OpenGL: " << versionGL;
 
 	inicializarTriangulo();
+	inicializarCuadrado();
 
-	const char* rutaVertexShader =
-		"VertexShader.shader";
-	const char* rutaFragmentShader =
-		"FragmentShader.shader";
+	const char* rutaVertexShader = "VertexShader.shader";
+	const char* rutaFragmentShader = "FragmentShader.shader";
 	shader = new Shader(rutaVertexShader, rutaFragmentShader);
 
 	//Mapeo de atributos
-	posicionID =
-		glGetAttribLocation(shader->getID(), "posicion");
-	colorID =
-		glGetAttribLocation(shader->getID(), "color");
+	posicionID = glGetAttribLocation(shader->getID(), "posicion");
+	colorID = glGetAttribLocation(shader->getID(), "color");
 
 	shader->desenlazar();
 
@@ -132,12 +163,24 @@ int main()
 	glEnableVertexAttribArray(posicionID);
 	glEnableVertexAttribArray(colorID);
 	//Especificar a OpenGL como comunicarse
-	glVertexAttribPointer(posicionID,
-		3, GL_FLOAT, GL_FALSE,
-		sizeof(Vertice), 0);
+	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+	glVertexAttribPointer(
+		colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3)
+	);
 
-
-
+	// Proceso de inicializar Vertex Array para el cuadrado
+	glGenVertexArrays(1, &vertexArrayCuadradoID);
+	glBindVertexArray(vertexArrayCuadradoID);
+	glGenBuffers(1, &bufferCuadradoID);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferCuadradoID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertice) * cuadrado.size(),
+		cuadrado.data(), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(posicionID);
+	glEnableVertexAttribArray(colorID);
+	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+	glVertexAttribPointer(
+		colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3)
+	);
 
 	//Ciclo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window)) {
@@ -147,8 +190,7 @@ int main()
 		//Valores RGBA
 		glClearColor(1, 0.8, 0, 1);
 		//Borrar!
-		glClear(GL_COLOR_BUFFER_BIT |
-			GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//Actualizar valores y dibujar
 		dibujar();
